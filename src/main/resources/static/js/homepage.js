44bb846362e494c9dbfac3f9fc6b830c0b5d3a1e
Vivian <1030518209@qq.com>
2018-03-04 20:11:17 +0800

    <dev>: homepage初稿

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
     $('.more').click(function () {
         var name = $(this).attr('id');
-        console.log(name);
         var navName = name.substring(0,name.indexOf('-'));
-        console.log(navName);
         var navId = navName + '-' + 'title';
-        console.log(navId);
         $('#navbar li.active').removeClass('active');
         var li = $("[id=" + navId + "]");
-        console.log(li.hasClass('active'));
         li.addClass('active');
-        console.log(li.hasClass('active'));
     });
+    $('#info').click(function () {
+        $('#navbar li.active').removeClass('active');
+        $(this).addClass('active');
+    })
 });
